d03137ab2f6573c8288520eb42e4f694aaa7819e
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-08-30 02:10:40 +0100

    Added temporary values to environment.ts for ui

--- constancy-ui/src/environments/environment.prod.ts
+++ constancy-ui/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   JENKINS_SERVICE_API_URL: 'https://api.sparrow.express',
   RAW_API_URL: 'https://api.sparrow.express',
+  GATEWAY_API_RAW_URL: 'http://localhost:8082',
 };
--- constancy-ui/src/environments/environment.stage.ts
+++ constancy-ui/src/environments/environment.stage.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   JENKINS_SERVICE_API_URL: 'https://api.staging.sparrow.express',
   RAW_API_URL: 'https://api.staging.sparrow.express',
+  GATEWAY_API_RAW_URL: 'http://localhost:8082',
 };

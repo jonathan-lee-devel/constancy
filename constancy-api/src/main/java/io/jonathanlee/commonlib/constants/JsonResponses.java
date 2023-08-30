30a2d5e93e885dc8668f882628f9499ea49c978c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-08-30 01:53:47 +0100

    Added common-lib, docker-compose.yml, and gradle.yml

---
+++ constancy-api/src/main/java/io/jonathanlee/commonlib/constants/JsonResponses.java
@@ -0,0 +1,9 @@
+package io.jonathanlee.commonlib.constants;
+
+public class JsonResponses {
+
+  private JsonResponses() {}
+
+  public static final String DEFAULT_ERROR_RESPONSE = "{\"error\":\"An error has occurred\"}";
+
+}
---
+++ constancy-api/src/main/java/io/jonathanlee/commonlib/constants/UserAttributes.java
@@ -0,0 +1,9 @@
+package io.jonathanlee.commonlib.constants;
+
+public class UserAttributes {
+
+  private UserAttributes() {}
+
+  public static final String UNDEFINED = "undefined";
+
+}

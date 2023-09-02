efe363a85459eb2e84a96c318abb834dbc7d2d69
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-09-02 20:19:00 +0100

    Updated UI environment values

--- constancy-ui/src/environments/environment.prod.ts
+++ constancy-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.sparrow.express',
-  RAW_API_URL: 'https://api.sparrow.express',
+  API_URL: 'https://api.constancy.live',
+  RAW_API_URL: 'https://api.constancy.live',
   GATEWAY_API_RAW_URL: 'http://localhost:8081',
 };
--- constancy-ui/src/environments/environment.stage.ts
+++ constancy-ui/src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.staging.sparrow.express',
-  RAW_API_URL: 'https://api.staging.sparrow.express',
+  API_URL: 'https://api.staging.constancy.live',
+  RAW_API_URL: 'https://api.staging.constancy.live',
   GATEWAY_API_RAW_URL: 'http://localhost:8081',
 };

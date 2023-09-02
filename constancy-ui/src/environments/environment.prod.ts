2c92d36e5593cb3c5af12cbd4f7aa72c92684019
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-09-02 02:05:19 +0100

    Updated UI environment and proxy values, updated landing page text

--- constancy-ui/src/environments/environment.prod.ts
+++ constancy-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   JENKINS_SERVICE_API_URL: 'https://api.sparrow.express',
   RAW_API_URL: 'https://api.sparrow.express',
-  GATEWAY_API_RAW_URL: 'http://localhost:8082',
+  GATEWAY_API_RAW_URL: 'http://localhost:8081',
 };
--- constancy-ui/src/environments/environment.stage.ts
+++ constancy-ui/src/environments/environment.stage.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   JENKINS_SERVICE_API_URL: 'https://api.staging.sparrow.express',
   RAW_API_URL: 'https://api.staging.sparrow.express',
-  GATEWAY_API_RAW_URL: 'http://localhost:8082',
+  GATEWAY_API_RAW_URL: 'http://localhost:8081',
 };

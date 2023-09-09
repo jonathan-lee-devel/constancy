c65837318337bcf4d3b35715d8054abc1fc2ae4f
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-09-09 23:46:57 +0100

    Added counter

--- constancy-ui-svelte/src/routes/+page.svelte
+++ constancy-ui-svelte/src/routes/+page.svelte
@@ -1,5 +1,10 @@
+<script>
+    import Counter from "./Counter.svelte";
+</script>
+
 <div class="container pt-5 justify-center">
     <h1 class="text-3xl font-bold underline-offset-2">Welcome to Constancy</h1>
+    <Counter />
 </div>
 
 <style lang="postcss">
